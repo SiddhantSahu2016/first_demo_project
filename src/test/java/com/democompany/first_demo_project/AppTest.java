62202961c053ae03d82ecb06833824c803801566
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2026-07-23 00:25:54 +0530

    This is my first Commit

---
+++ src/test/java/com/democompany/first_demo_project/AppTest.java
@@ -0,0 +1,16 @@
+package com.democompany.first_demo_project;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest(classes = App.class)
+public class AppTest {
+	@Test
+	void test() {
+		System.out.println("Hello World");
+	}
+}
