62a72f9b6978b0869f14eff12ae0e957fd633718
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2026-07-23 02:56:30 +0530

    add the start file

--- src/main/java/com/democompany/first_demo_project/App.java
+++ src/main/java/com/democompany/first_demo_project/App.java
@@ -2,10 +2,16 @@ package com.democompany.first_demo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
