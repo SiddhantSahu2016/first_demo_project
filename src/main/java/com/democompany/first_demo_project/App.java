8c2c0cafc7a4eac3a404b38bf9da3687577fb483
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2026-07-23 02:22:23 +0530

    creating my first web using maven and AWS

--- src/main/java/com/democompany/first_demo_project/App.java
+++ src/main/java/com/democompany/first_demo_project/App.java
@@ -1,14 +1,11 @@
 package com.democompany.first_demo_project;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
